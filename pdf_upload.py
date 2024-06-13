1c2370935cd09d58b077b99018661e1d235b654c
大谷瞭太 <rooya@fourmix.co.jp>
2024-06-13 11:26:18 +0900

    一旦コミット

--- pdf_upload.py
+++ pdf_upload.py
@@ -1,16 +1,22 @@
+# pip install pycryptodome
+import os
+from glob import glob
 import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 
 from PyPDF2 import PdfReader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Qdrant
+from langchain.chains import RetrievalQA
 
 from qdrant_client import QdrantClient
 from qdrant_client.models import Distance, VectorParams
 
 QDRANT_PATH = "./local_qdrant"
-COLLECTION_NAME = "my_collection"
+COLLECTION_NAME = "my_collection_2"
 
 
 def init_page():
@@ -22,6 +28,20 @@ def init_page():
     st.session_state.costs = []
 
 
+def select_model():
+    model = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-3.5-16k", "GPT-4"))
+    if model == "GPT-3.5":
+        st.session_state.model_name = "gpt-3.5-turbo"
+    elif model == "GPT-3.5":
+        st.session_state.model_name = "gpt-3.5-turbo-16k"
+    else:
+        st.session_state.model_name = "gpt-4"
+
+    # 300: 本文以外の指示のトークン数 (以下同じ)
+    st.session_state.max_token = OpenAI.modelname_to_contextsize(st.session_state.model_name) - 300
+    return ChatOpenAI(temperature=0, model_name=st.session_state.model_name)
+
+
 def get_pdf_text():
     uploaded_file = st.file_uploader(
         label='Upload your PDF here😇',
@@ -31,11 +51,11 @@ def get_pdf_text():
         pdf_reader = PdfReader(uploaded_file)
         text = '\n\n'.join([page.extract_text() for page in pdf_reader.pages])
         text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-            model_name=st.session_state.emb_model_name,
+            model_name="text-embedding-ada-002",
             # 適切な chunk size は質問対象のPDFによって変わるため調整が必要
             # 大きくしすぎると質問回答時に色々な箇所の情報を参照することができない
             # 逆に小さすぎると一つのchunkに十分なサイズの文脈が入らない
-            chunk_size=250,
+            chunk_size=500,
             chunk_overlap=0,
         )
         return text_splitter.split_text(text)
@@ -44,7 +64,10 @@ def get_pdf_text():
 
 
 def load_qdrant():
-    client = QdrantClient(path=QDRANT_PATH)
+    client = QdrantClient(
+        url=os.environ['QDRANT_CLOUD_ENDPOINT'],
+        api_key=os.environ['QDRANT_CLOUD_API_KEY']
+    )
 
     # すべてのコレクション名を取得
     collections = client.get_collections().collections
@@ -76,8 +99,25 @@ def build_vector_store(pdf_text):
     #     pdf_text,
     #     OpenAIEmbeddings(),
     #     path="./local_qdrant",
-    #     collection_name=COLLECTION_NAME,
+    #     collection_name="my_documents",
     # )
+
+
+def build_qa_model(llm):
+    qdrant = load_qdrant()
+    retriever = qdrant.as_retriever(
+        # "mmr",  "similarity_score_threshold" などもある
+        search_type="similarity",
+        # 文書を何個取得するか (default: 4)
+        search_kwargs={"k": 10}
+    )
+    return RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=retriever,
+        return_source_documents=True,
+        verbose=True
+    )
 
 
 def page_pdf_upload_and_build_vector_db():
@@ -90,11 +130,38 @@ def page_pdf_upload_and_build_vector_db():
                 build_vector_store(pdf_text)
 
 
+def ask(qa, query):
+    with get_openai_callback() as cb:
+        # query / result / source_documents
+        answer = qa(query)
+
+    return answer, cb.total_cost
+
+
 def page_ask_my_pdf():
     st.title("Ask My PDF(s)")
-    st.write('Under Construction')
 
-    # 後で実装する
+    llm = select_model()
+    container = st.container()
+    response_container = st.container()
+
+    with container:
+        query = st.text_input("Query: ", key="input")
+        if not query:
+            answer = None
+        else:
+            qa = build_qa_model(llm)
+            if qa:
+                with st.spinner("ChatGPT is typing ..."):
+                    answer, cost = ask(qa, query)
+                st.session_state.costs.append(cost)
+            else:
+                answer = None
+
+        if answer:
+            with response_container:
+                st.markdown("## Answer")
+                st.write(answer)
 
 
 def main():
@@ -111,6 +178,7 @@ def main():
     st.sidebar.markdown(f"**Total cost: ${sum(costs):.5f}**")
     for cost in costs:
         st.sidebar.markdown(f"- ${cost:.5f}")
+
 
 if __name__ == '__main__':
     main()
